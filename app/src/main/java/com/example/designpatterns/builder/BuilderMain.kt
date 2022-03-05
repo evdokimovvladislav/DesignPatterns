5b501dc6134b46b9679d5933f2c3be2c6e3c60d8
Mjko-1 <evdokimoff.vlad2011@yandex.ru>
2022-03-05 13:43:46 +0300

    Builder.

---
+++ app/src/main/java/com/example/designpatterns/builder/BuilderMain.kt
@@ -0,0 +1,19 @@
+package com.example.patterns.creational.builder
+
+/**
+ * @author Evdokimov on 03.03.2022.
+ *
+ * Паттерн для пошагового создания сложных объектов при помощи цепочки методов.
+ *
+ * Не понимаю зачем нужен в Kotlin при наличии дефолтных значений в конструкторе.
+ */
+
+fun main() {
+    val house = HouseBuilder()
+        .buildWalls("Some walls producer")
+        .buildDoor("Some doors producer")
+        .buildWindow("Some window producer")
+        .build()
+
+    println(house)
+}
---
+++ app/src/main/java/com/example/designpatterns/builder/HouseBuilder.kt
@@ -0,0 +1,39 @@
+package com.example.patterns.creational.builder
+
+/**
+ * @author Evdokimov on 04.03.2022.
+ */
+class HouseBuilder {
+
+    private var walls: String = "Default wall producer"
+    private var door: String = "Default door producer"
+    private var roof: String = "Default roof producer"
+    private var window: String = "Default window producer"
+
+    fun buildWalls(walls: String): HouseBuilder {
+        this.walls = walls
+        return this
+    }
+
+    fun buildDoor(door: String): HouseBuilder {
+        this.door = door
+        return this
+    }
+
+    fun buildRoof(roof: String): HouseBuilder {
+        this.roof = roof
+        return this
+    }
+
+    fun buildWindow(window: String): HouseBuilder {
+        this.window = window
+        return this
+    }
+
+    fun build() = House(
+        walls = walls,
+        door = door,
+        roof = roof,
+        window = window
+    )
+}
