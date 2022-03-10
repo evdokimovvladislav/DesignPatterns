05665a3190c7d0abe372b706c5877c228931e2d3
Mjko-1 <evdokimoff.vlad2011@yandex.ru>
2022-03-10 14:26:12 +0300

    DP-001: реализация паттерна builder
      -- добавил scope функцию apply

--- app/src/main/java/com/example/designpatterns/creational/builder/HouseBuilder.kt
+++ app/src/main/java/com/example/designpatterns/creational/builder/HouseBuilder.kt
@@ -10,25 +10,13 @@ class HouseBuilder {
     private var roof: String = "Default roof producer"
     private var window: String = "Default window producer"
 
-    fun buildWalls(walls: String): HouseBuilder {
-        this.walls = walls
-        return this
-    }
+    fun buildWalls(walls: String): HouseBuilder = apply { this.walls = walls }
 
-    fun buildDoor(door: String): HouseBuilder {
-        this.door = door
-        return this
-    }
+    fun buildDoor(door: String): HouseBuilder = apply { this.door = door }
 
-    fun buildRoof(roof: String): HouseBuilder {
-        this.roof = roof
-        return this
-    }
+    fun buildRoof(roof: String): HouseBuilder = apply { this.roof = roof }
 
-    fun buildWindow(window: String): HouseBuilder {
-        this.window = window
-        return this
-    }
+    fun buildWindow(window: String): HouseBuilder = apply { this.window = window }
 
     fun build() = House(
         walls = walls,
